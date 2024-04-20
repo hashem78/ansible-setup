ba49f42a4a56783f4675292175a48d2f6d6eb12e
hashem78 <hashem.olayano@gmail.com>
2024-04-20 21:40:48 +0300

    refactor: init.sh again

--- init.sh
+++ init.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-apt install sudo
-useradd -m -p "$y$j9T$Tzm8HVr66WHce3gssjOAs1$8U4YBva8dKLrleee34ftMfyQ20If4PYqZu0WdIYx2w1" -s /bin/bash mythi
-adduser mythi sudo
-sudo -H -u mythi apt update
-sudo -H -u mythi apt install -y sudo python3 python3-pip git
-sudo -H -u mythi bash -c 'pip3 install ansible' 
-su mythi
+# Install sudo if not already installed
+apt-get update
+apt-get install -y sudo
+
+# Create the user mythi with password 'password'
+useradd -m -p 'password' -s /bin/bash mythi
+
+# Add the user mythi to the sudo group
+usermod -aG sudo mythi
+
+# Update the package index and install required packages
+sudo -H -u mythi bash -c 'sudo apt-get update && sudo apt-get install -y python3 python3-pip git'
+
+# Install Ansible for the user mythi
+sudo -H -u mythi bash -c 'pip3 install ansible'
+
+# Switch to the user mythi
+su - mythi
