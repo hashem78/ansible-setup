6473c97e61cb6596d49c1c87ea9c0f4d1c808e87
hashem78 <hashem.olayano@gmail.com>
2024-04-20 21:35:48 +0300

    fix: add install sudo

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+apt install sudo
 useradd -m -p "$y$j9T$Tzm8HVr66WHce3gssjOAs1$8U4YBva8dKLrleee34ftMfyQ20If4PYqZu0WdIYx2w1" -s /bin/bash mythi
 adduser mythi sudo
 sudo -H -u mythi apt update
